03119b253b2f143e7d596590e1e63b9124c8249a
Joseph Ball <jball321@live.com>
2017-10-17 16:57:51 -0400

    Added routes in htmlRoutes.js. Also added navbar partial.

--- app/controllers/htmlRoutes.js
+++ app/controllers/htmlRoutes.js
@@ -11,9 +11,26 @@ router.get ('/', function(req, res) {
 
 router.get ('/about', function(req, res) {
 	const hbsObj = {
+		about: true,
 		title: 'Joseph Ball - Full Stack Developer'
 	};
 	res.render('about', hbsObj);
+});
+
+router.get ('/portfolio', function(req, res) {
+	const hbsObj = {
+		portfolio: true,
+		title: 'Joseph Ball - Full Stack Developer'
+	};
+	res.render('portfolio', hbsObj);
+});
+
+router.get ('/contact', function(req, res) {
+	const hbsObj = {
+		contact: true,
+		title: 'Joseph Ball - Full Stack Developer'
+	};
+	res.render('contact', hbsObj);
 });
 
 export default router;
