2672d255e7d75a395b20235c141800427b896bbd
Joseph Ball <jball321@live.com>
2018-01-18 22:02:13 -0500

    Added webpack to compile sass and bundle js files. Issues with sass compiling still need to be resolved.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
 var path = require('path');
-var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	entry: {
-		app: './client'
+		client: './client/index.js'
 	},
 	output: {
-		path: path.resolve(__dirname, './public/assets/js'),
-		filename: 'bundle.js'
+		path: path.resolve(__dirname, './public'),
+		filename: './js/bundle.js'
 	},
 	module: {
 		rules: [
 			{
-				include: [path.resolve(__dirname, './client/scripts')],
-				test: /\.js$/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['es2015', 'react']
-					}
-				}
-			},
+				include: [path.resolve(__dirname, './client/scss/main.scss')],
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					use: ['css-loader', 'resolve-url-loader', 'sass-loader?sourceMap'],
+					fallback: 'style-loader'
+				})
+			}
 		]
 	},
+	plugins: [
+		new ExtractTextPlugin({
+			filename: './css/style.css'
+		})
+	],
 	devtool: 'eval-source-map'
 }
